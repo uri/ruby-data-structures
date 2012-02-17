34b160623e9bd2e1e3ff7b0ee3f587095a9ff191
Uri Gorelik <ugorelik@Uri-Goreliks-MacBook-Pro.local>
2012-02-17 09:51:40 -0500

    revised heap a little

--- heap/heap.rb
+++ heap/heap.rb
@@ -4,72 +4,80 @@
 
 class Heap
 	attr_reader :heap_list
-	
-	def initialize
-		@heap_list = []
-	end
-	
-	def initialize array
-		@heap_list = []
-		
-		@given_list = Array.new array
-		# Run operations to make this a heap
-		
-		for i in (0..@given_list.length - 1)
-			insert @given_list[i]
+
+	def initialize params
+    @heap_list = []
+  
+		if (params[:list] != nil)
+
+			if params[:better_build]
+				better_build params[:list]
+			else
+				a = params[:list]
+				a.each do |i|
+          insert i
+				end
+			end
 		end
 	end
-	
+
+
+
 	def insert x
 		@heap_list << x
 		swap_parent(@heap_list.length - 1)
 	end
-	
-	
+
+
 	def sort!
 		a = []
 		for i in (0..@heap_list.length - 1)
 			a << extract_max!
 		end
-		
+
 		return a
 	end
-	
+
 	def get_max
 		@heap_list[0]
 	end
-	
-	
-	
+
+
+
 	def increase_key list, index, value
 		return if list[index] >= value
 		# Continue
 	end
-	
+
 	# ===== Private ===== #
 	private
 	def extract_max!
 		ret_largest = @heap_list.shift
-		
+
 		last = @heap_list.pop
 		@heap_list.insert(0, last)
-		
+
 		# Fix the heap property
 		heapify 0
-		
+
 		ret_largest
-		
+
 	end
-	
-	
-	
+
+	def better_build array
+		@heap_list = array
+		for i in (((@heap_list.length / 2) - 1).downto 0)
+			heapify i
+		end
+	end
+
 	def heapify index
 		left = (index * 2) + 1
 		right = (index * 2) + 2
 		largest = nil
-		
+
 		a = @heap_list
-		
+
 		if left <= (a.length - 1) && a[left] > a[index]
 			largest = left
 		else
@@ -79,32 +87,65 @@ class Heap
 		if right <= (a.length - 1) && a[right] > a[largest]
 			largest = right
 		end
-		
+
 		if largest != index
 			a[index], a[largest] = a[largest], a[index]
 			heapify(largest)
 		end
-			
-		
-		
-		
+
+
+
+
 	end
-	
+
 	def swap_parent i
 		a = @heap_list
 		if (a[i] > a[i/2])
-			
+
 			a[i], a[i/2] = a[i/2], a[i]
-			
+
 			# Keep going if we need to
-			swap_parent (i/2)
+			swap_parent((i/2))
 		end
 	end
-	
-		
+
+
 end
 
-a = Array.new(10) { rand 1001 }
-h = Heap.new a
 
-puts "This is the sorted array\n #{h.sort!}"
+require "algorithms"
+include Algorithms
+
+def time_it
+  t1 = Time.now
+  a = Array.new(10000) { rand 100 }
+  
+  yield(a)
+  
+  puts "It took #{Time.now - t1} seconds."
+end
+
+# Using default sort
+time_it { |a|
+  a.sort!
+}
+
+
+# Using better build
+time_it { |a|
+  h2 = Heap.new :list => a, :better_build => true
+  #puts "This is better build array\n #{h2.sort!}"
+}
+
+# Using old method
+time_it { |a|
+  h1 = Heap.new :list => a
+  #puts "This is the heap array\n #{h1.sort!}"
+}
+
+
+
+
+time_it { |a|
+  Sort.quicksort a
+}
